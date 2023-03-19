0247047983eb00099e3cf06f5b0ffc6e5ba465a5
Olivier Livet <olivierlivet@MacBook-Air-de-Olivier.local>
2023-03-19 13:44:56 +0100

    adding CMS management

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,29 @@ import alpinejs from "@astrojs/alpinejs";
 
 import tailwind from "@astrojs/tailwind";
 
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     alpinejs(),
-    tailwind({
+    tailwind({}),
+
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        // Add your components here
+        question: 'storyblok/question',
+
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+      },
     })
+
   ]
 });
