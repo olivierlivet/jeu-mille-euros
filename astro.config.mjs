235104ef9b31a88f94bafb11e1854c6dfc1f107a
Olivier Livet <olivierlivet@MacBook-Air-de-Olivier.local>
2023-03-23 21:54:20 +0100

    Work on astro site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,22 @@
 import { defineConfig } from 'astro/config';
 import alpinejs from "@astrojs/alpinejs";
-
 import tailwind from "@astrojs/tailwind";
-
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
-
+import compress from "astro-compress";
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    alpinejs(),
-    tailwind({}),
-
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        // Add your components here
-        question: 'storyblok/question',
-
-      },
-      apiOptions: {
-        // Choose your Storyblok space region
-      },
-    })
-
-  ]
+  integrations: [alpinejs(), tailwind({}), storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      // Add your components here
+      question: 'storyblok/question'
+    },
+    apiOptions: {
+      // Choose your Storyblok space region
+    }
+  }), compress()]
 });
